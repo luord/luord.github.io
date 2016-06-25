790662d8a8c5ef40983e2003ee3120ac55786ad7
Luis Orduz <info@luord.com>
2016-06-25 14:13:27 -0500

    added extra settings

---
+++ settings/me.py
@@ -0,0 +1,19 @@
+AUTHOR = 'Luis Orduz'
+SITENAME = 'Luis Orduz'
+SITEURL = ''
+USER_LOGO_URL = '/assets/img/site/favicon.png'
+HOSTNAME = 'luord.com'
+
+# Blogroll
+LINKS = (('Enter', '//enter.co'),
+         ('Ars Technica', '//arstechnica.com'),
+         ('FayerWayer', '//fayerwayer.com'),
+         ('OSNews', '//osnews.com'),
+         ('Forbes', '//forbes.com/most-popular/'),)
+
+# Social widget
+SOCIAL = (('LinkedIn', '//co.linkedin.com/in/luis-orduz-57a46138'),
+          ('Twitter', '//twitter.com/luord'),
+          ('Facebook', '//facebook.com/luord'),
+          ('IMDB', '//www.imdb.com/user/ur39224109/'),)
+
