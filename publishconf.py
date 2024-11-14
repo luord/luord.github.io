72042d9bfce000274af527752d7ac5a51cea9b8e
Luis Orduz <lo@luord.com>
2024-11-14 13:50:58 -0500

    updated newsletter endpoint

--- publishconf.py
+++ publishconf.py
@@ -19,7 +19,7 @@ FEED_MAX_ITEMS = 10
 CATEGORY_FEED_ATOM = 'feed/{slug}.atom.xml'
 
 NEWSLETTER_URL = 'pages/newsletter'
-NEWSLETTER_ENDPOINT = 'https://luord-newsletter.web.val.run'
+NEWSLETTER_ENDPOINT = 'https://luord-newsletter.web.val.run/subscribe'
 
 # Following items are often useful when publishing
 
